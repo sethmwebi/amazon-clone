e3f6b98f5c293ea07be05d19fab37579647e492e
Seth Mwebi <sethmwebi27@gmail.com>
2022-01-10 15:16:38 +0300

    day 2 build

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDX19kCA6jrFJ8wpaW39hJYcW4N_E-ohE8",
+  authDomain: "clone-78ffb.firebaseapp.com",
+  projectId: "clone-78ffb",
+  storageBucket: "clone-78ffb.appspot.com",
+  messagingSenderId: "178974411888",
+  appId: "1:178974411888:web:1eef0c96e5d8e437d1f8b5"
+};
